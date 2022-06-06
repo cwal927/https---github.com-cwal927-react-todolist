c6dfe3f5017ee039bb0f1e1305fe5334510d87f2
kyo <cwal927@naver.com>
2022-06-07 00:07:11 +0900

    TodoList 기능 완료

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,15 +4,9 @@ import './TodoList.css';
 
 function TotoList(){
 
-    const [List,setList] = useState([{
-        id:0,
-        text:'',
-        state:true
-    }]
-    );
+    const [List,setList] = useState([]);
     const [nextId, setNextId] = useState(List.length+1);
     const [inputText,setInputText] = useState('');
-
     const addInputTextEvent = () => {
         const newStrMap = List.concat({
             id: nextId,
@@ -32,30 +26,34 @@ function TotoList(){
         setList(newStrMap);
     }
 
+    /*체크 하면 style 주기*/
+    const checkHandler = ({target}) => {
+        checkedItemHandler(target.parentNode, target.value,target.checked);
+    }
+
+    const checkedItemHandler = (box,id,isChecked) => {
+        if(isChecked){
+            box.style.textDecoration="line-through";
+            box.style.color="gray";
+        }else {
+            box.style.textDecoration="none";
+            box.style.color="black";
+        }
+    }
+
     const mapList = List.map(item =>
                                 <li key={item.id}> 
-                                    <input type="checkbox" key={item.id}/>
+                                    <input type="checkbox" key={item.id} onChange={checkHandler}/>
                                     <label for={item.id}><span></span>{item.text} </label> 
                                     <button onClick={()=>deleteListEvent(item.id)}>삭제</button>
                                 </li>)
-
-
     return(
-        <div className='wrap'>
-            <div className='todoListDiv'>
-                <div>
-                    <h1>일정관리</h1>
-                </div>
-                <div>
-                    <input className="todoListInput" type='text' id='contents' name='contents' value={inputText} onChange={inputChangeEvent}></input>
-                    <button value="" className="todoListButton" onClick={addInputTextEvent}>추가</button>
-                </div>
-                <div>
-                    <ul id='list'>
-                        {mapList}
-                    </ul>
-                </div>
-            </div>
+        <div>
+            <input className="todoListInput" type='text' id='contents' name='contents' placeholder="할일을 입력해 주세요." value={inputText} onChange={inputChangeEvent}></input>
+            <button value="" className="todoListButton" onClick={addInputTextEvent}>+</button>
+            <ul>
+                {mapList}
+            </ul>
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
-import TotoList from './containers/TodoList';
+import App from './containers/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <TotoList />
+    <App />
   </React.StrictMode>
 );
 
